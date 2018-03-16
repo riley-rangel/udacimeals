cf090e850fb555234ecad5bde773a108a8687521
Riley Rangel <rrangel91@gmail.com>
2018-03-15 18:19:04 -0700

    Create store using reducer function.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { createStore } from 'redux'
 import './index.css'
 import App from './components/App'
+import reducer from './reducers'
+
+const store = createStore(reducer)
+
+console.log(store.getState())
 
 ReactDOM.render(<App />, document.getElementById('root'))

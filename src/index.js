be7c4bc853262f8a66fead08c3350506177dbe2c
Riley Rangel <rrangel91@gmail.com>
2018-03-16 15:55:55 -0700

    Add redux devtools extension to store creation.

--- src/index.js
+++ src/index.js
@@ -5,7 +5,10 @@ import './index.css'
 import App from './components/App'
 import reducer from './reducers'
 
-const store = createStore(reducer)
+const store = createStore(
+  reducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+)
 
 console.log(store.getState())
 

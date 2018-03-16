30afb309db1d864394a40f5aebc187a98840d3b3
Riley Rangel <rrangel91@gmail.com>
2018-03-15 18:08:36 -0700

    Add reducer switch statement to return updated state based upon an action.

---
+++ src/reducers/index.js
@@ -0,0 +1,67 @@
+import {
+  ADD_RECIPE,
+  REMOVE_FROM_CALENDAR
+} from '../actions'
+
+const initialCalendarState = {
+  monday: {
+    breakfast: null,
+    lunch: null,
+    dinner: null
+  },
+  tuesday: {
+    breakfast: null,
+    lunch: null,
+    dinner: null
+  },
+  wednesday: {
+    breakfast: null,
+    lunch: null,
+    dinner: null
+  },
+  thursday: {
+    breakfast: null,
+    lunch: null,
+    dinner: null
+  },
+  friday: {
+    breakfast: null,
+    lunch: null,
+    dinner: null
+  },
+  saturday: {
+    breakfast: null,
+    lunch: null,
+    dinner: null
+  },
+  sunday: {
+    breakfast: null,
+    lunch: null,
+    dinner: null
+  }
+}
+
+export default function calendar(state = initialCalendarState, action) {
+  const { day, meal, recipe } = action
+
+  switch(action.type) {
+    case ADD_RECIPE:
+      return {
+        ...state,
+        [day]: {
+          ...state[day],
+          [meal]: recipe.label
+        }
+      }
+    case REMOVE_FROM_CALENDAR:
+      return {
+        ...state,
+        [day]: {
+          ...state[day],
+          [meal]: null
+        }
+      }
+    default:
+        return state
+  }
+}

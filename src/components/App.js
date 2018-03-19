d80f4d06cf415eecbe1df9ce0a8e3fe622cadd39
Riley Rangel <rrangel91@gmail.com>
2018-03-19 15:13:59 -0700

    Use react-redux connect method to map state to props inside App component.

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
 
-export default class App extends Component {
+class App extends Component {
   render() {
+    console.log(this.props)
     return (
       <div>
         Back to nothing...
@@ -9,3 +11,22 @@ export default class App extends Component {
     )
   }
 }
+
+function mapStateToProps(calendar) {
+  const dayOrder = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday']
+
+  return {
+    calendar: dayOrder.map(day => ({
+      day,
+      meals: Object.keys(calendar[day]).reduce((meals, meal) => {
+        meals[meal] = calendar[day][meal]
+          ? calendar[day][meal]
+          : null
+
+        return meals
+      }, {})
+    }))
+  }
+}
+
+export default connect(mapStateToProps)(App)

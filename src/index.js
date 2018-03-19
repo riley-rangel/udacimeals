db66241edfe4f4acd18637d26b04d405eb739410
Riley Rangel <rrangel91@gmail.com>
2018-03-19 12:34:00 -0700

    Add react-redux package; wrap App with Provider component.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { createStore } from 'redux'
+import { Provider } from 'react-redux'
 import './index.css'
 import App from './components/App'
 import reducer from './reducers'
@@ -10,6 +11,9 @@ const store = createStore(
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 )
 
-console.log(store.getState())
-
-ReactDOM.render(<App store={store}/>, document.getElementById('root'))
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+)

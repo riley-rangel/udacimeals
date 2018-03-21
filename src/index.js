54f7223a4ff2834eaf22de719728666cf2c6cfab
Riley Rangel <rrangel91@gmail.com>
2018-03-21 13:30:57 -0700

    Add redux-thunk middleware to store.

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { createStore, applyMiddleware, compose } from 'redux'
 import { Provider } from 'react-redux'
+import thunk from 'redux-thunk'
 import './index.css'
 import App from './components/App'
 import reducer from './reducers'
@@ -18,12 +19,14 @@ const logger = store => next => action => {
   return result
 }
 
+const middleware = [logger, thunk]
+
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 
 const store = createStore(
   reducer,
   composeEnhancers(
-    applyMiddleware(logger)
+    applyMiddleware(...middleware)
   )
 )
 
